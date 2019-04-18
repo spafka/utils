47797f580a89282c9c965b924e3eda76f3b6d804
wanghf <wanghf@iovdc.cn>
2019-04-18 16:44:13 +0800

    up add licence

--- src/main/java/io/github/spafka/hex/XorUtils.java
+++ src/main/java/io/github/spafka/hex/XorUtils.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 package io.github.spafka.hex;
 
 import com.google.common.base.Preconditions;
@@ -8,7 +26,7 @@ import java.util.Arrays;
 
 @Slf4j
 public class XorUtils {
-   static BitAndByteUtil b2b = new BitAndByteUtil();
+    static BitAndByteUtil b2b = new BitAndByteUtil();
 
     public static String  xorAtFixPosit(@NonNull String s1, @NonNull String s2, int poisition) {
 
@@ -41,7 +59,6 @@ public class XorUtils {
 
     // 把8位字节拼成byte数组
     public static byte[] cheng8bitString2Bytes(String string) {
-
 
 
         Preconditions.checkState(string.length() % 8 == 0);
--- src/main/java/io/github/spafka/http/OkHttpUtils.java
+++ src/main/java/io/github/spafka/http/OkHttpUtils.java
@@ -1,3 +1,23 @@
+
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 package io.github.spafka.http;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +35,6 @@ import java.util.concurrent.TimeUnit;
  * add okhttp
  * <p>只实现了httpget 和 application/json 提交的方式</P>
  */
-
 @Slf4j
 public class OkHttpUtils {
 
